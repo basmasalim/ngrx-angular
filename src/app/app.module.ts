99b2ac6b803719d23fe80cd46fbc7a0c6bf135de
basmasalim <basmasalim030@gmail.com>
2025-07-28 14:50:55 +0300

    chore: update dependencies and fix formatting issues
    - Updated @angular/cdk to version 17.3.10
    - Updated @ngrx/store to version 17.2.0
    - Updated express to version 4.21.2
    - Updated primeng to version 17.18.15
    - Updated @types/express to version 4.17.23
    - Fixed formatting issue in app.component.html
    - Cleaned up imports and formatting in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,47 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule, isDevMode} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, isDevMode } from "@angular/core";
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
-import {RouterModule, Routes} from '@angular/router';
-import {AuthModule} from './auth/auth.module';
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { reducers, metaReducers, reducers, metaReducers } from './reducers';
+import { RouterModule, Routes } from "@angular/router";
+import { AuthModule } from "./auth/auth.module";
+import { SidebarModule } from "primeng/sidebar";
+import { ButtonModule } from "primeng/button";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { reducers, metaReducers } from "./reducers";
 
 const routes: Routes = [
-    {
-        path: 'blogs',
-        loadChildren: () => import('./blogs/blogs.module').then(m => m.BlogModule)
-        },
-    {
-        path: '**',
-        redirectTo: '/'
-    }
+  {
+    path: "blogs",
+    loadChildren: () =>
+      import("./blogs/blogs.module").then((m) => m.BlogModule),
+  },
+  {
+    path: "**",
+    redirectTo: "/",
+  },
 ];
 
-
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        RouterModule.forRoot(routes, {}),
-        HttpClientModule,
-        SidebarModule,
-        ButtonModule,
-        AuthModule.forRoot(),
-        StoreModule.forRoot({}, {}),
-        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-        StoreModule.forRoot(reducers, {
-      metaReducers
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, {}),
+    HttpClientModule,
+    SidebarModule,
+    ButtonModule,
+    AuthModule.forRoot(),
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
     }),
-    ],
-    bootstrap: [AppComponent]
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
